c21e0982c5164b6e77feb8cf9840ed3d5b1a082d
quiryat zavala <qzavala2014@gmail.com>
2024-03-28 16:11:34 -0700

    added h1 atgs

--- capstone-project/src/Pages/Cart.jsx
+++ capstone-project/src/Pages/Cart.jsx
@@ -3,7 +3,9 @@ import React from "react"
 
 const Cart = () => {
     return (
-        <div>Cart Page</div>
+        <div>
+            <h1>Cart </h1>
+        </div>
     )
 }
 export default Cart
--- capstone-project/src/Pages/Login.jsx
+++ capstone-project/src/Pages/Login.jsx
@@ -3,10 +3,11 @@ import React from "react"
 const Login = () => {
     return (
         <div>
+            <h1>Login!!</h1>
             <form>
-                <label type="email">Email</label>
+                <label type="email">Email:</label>
                 <input type="email" placeholder="pokemon@example.com" />
-                <label type="password">Password</label>
+                <label type="password">Password:</label>
                 <input type="password" placeholder="password" />
             </form>
         </div>
