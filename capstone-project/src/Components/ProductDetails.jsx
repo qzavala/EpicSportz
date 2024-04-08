adab13c0144acacfd784d0df433c9c6a6803f759
quiryat zavala <qzavala2014@gmail.com>
2024-04-08 11:17:28 -0700

    updated css

--- capstone-project/src/Components/ProductDetails.jsx
+++ capstone-project/src/Components/ProductDetails.jsx
@@ -12,12 +12,12 @@ const ProductDetails = ({ product, isSingle }) => {
   return (
     <div className="product">
       <img src={product.image} alt={product.title} className="product-image" />
-      <div className="card-content">
-        <h2 className="card-title">{product.title}</h2>
-        <p className="card-price">${product.price}</p>
+      <div className="product-content">
+        <h2 className="product-title">{product.title}</h2>
+        <p className="product-price">${product.price}</p>
         <p>{product.category}</p>
-        {isSingle && <p className="card-description">{product.description}</p>}
-        <button className="card-button">Add to Cart</button>
+        {isSingle && <p className="product-description">{product.description}</p>}
+        <button className="product-button">Add to Cart</button>
         {!isSingle && (
           <button onClick={handleViewItemClick} className="view-item-button">
             View Item
